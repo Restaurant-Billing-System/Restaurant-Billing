b1482615ae658e173559c6d7a089d81ce1ae01fa
Madhavi Boini <81870030+nnectar@users.noreply.github.com>
2021-07-15 17:23:04 +0530

    Update updateItem.java

--- src/updateItem.java
+++ src/updateItem.java
@@ -2,16 +2,7 @@ import java.sql.*;
 import Project.ConnectionPrivider;
 import javax.swing.JOptionPane;
 import net.proteanit.sql.DbUtils;
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author Narasimha
- */
 public class updateItem extends javax.swing.JFrame {
 
     /**
