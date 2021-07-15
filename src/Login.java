b78663f495924e04c8e0d2472e4c0705edf01d7f
Madhavi Boini <81870030+nnectar@users.noreply.github.com>
2021-07-15 17:21:37 +0530

    Update Login.java

--- src/Login.java
+++ src/Login.java
@@ -1,16 +1,7 @@
 
 import javax.swing.JOptionPane;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author Narasimha
- */
 public class Login extends javax.swing.JFrame {
     /**
      * Creates new form Login
